58b66e64d9c9712e5475f9c18a7bcabf2aeba6b9
Daniel Hipp <d.hipp@gmx.net>
2018-12-13 17:19:35 +0100

    first commit

--- app/src/main/java/de/computing/mobile/androidapp_mobilecomputing/StartActivity.java
+++ app/src/main/java/de/computing/mobile/androidapp_mobilecomputing/StartActivity.java
@@ -1,7 +1,10 @@
 package de.computing.mobile.androidapp_mobilecomputing;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class StartActivity extends AppCompatActivity {
 
@@ -9,5 +12,22 @@ public class StartActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+
+        //Button OnClick Listeners >>>>
+        Button buttonText = findViewById(R.id.start_button_text);
+        buttonText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(StartActivity.this, TextActivity.class));
+            }
+        });
+
+        Button buttonImage = findViewById(R.id.start_button_image);
+        buttonImage.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                startActivity(new Intent(StartActivity.this, PictureActivity.class));
+            }
+        }); //<<<<
     }
 }
