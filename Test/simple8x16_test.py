6a351658f5824112a93a2eaef8f6f7ad35d9954f
thgnaedi <thgnaedi@htwg-konstanz.de>
2018-12-15 19:33:12 +0000

    simple testings for matrix

--- Test/simple8x16_test.py
+++ Test/simple8x16_test.py
@@ -1,4 +1,5 @@
 import time
+import numpy as np
 
 from PIL import Image
 from PIL import ImageDraw	
@@ -8,7 +9,7 @@ from Adafruit_LED_Backpack import Matrix8x16
 
 
 def draw_img(display, np_array):
-	print(np_array.shape)
+	assert(np_array.shape == (8,16))
 	display.clear()
 	# Run through each pixel individually and turn it on.
 	for x in range(8):
@@ -16,9 +17,19 @@ def draw_img(display, np_array):
 			display.set_pixel(x, y, np_array[x,y])
 	display.write_display()
 	return
+
+def draw_periodic(display,full_array,times=200):
+	max = full_array.shape[1]
+	array = np.zeros((8,16))
+	for i in range(times):
+		for k in range(16):
+			array[:,k] = full_array[:,(k+i) % max]
+		draw_img(display, array)
+		time.sleep(0.125)
+	return
+
 	
-	
-display = Matrix8x16.Matrix8x16(adress=0x70)
+display = Matrix8x16.Matrix8x16(address=0x70)
 display.begin()
 
 testIcon = np.array([[0,0,0,0,1,1,1,0,0,0,0,0,0,0,0,0],
@@ -30,4 +41,15 @@ testIcon = np.array([[0,0,0,0,1,1,1,0,0,0,0,0,0,0,0,0],
 					 [0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0],
 					 [0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1]])
 
-draw_img(display, testIcon)
+#draw_img(display, testIcon)
+
+full_sine = np.array([[0,0,0,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+			[0,1,1,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0],
+			[1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0],
+			[0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,1],
+			[0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,1],
+			[0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1,0],
+			[0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,1,1,0,0],
+			[0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,0,0,0,0]])
+
+draw_periodic(display, full_sine)
