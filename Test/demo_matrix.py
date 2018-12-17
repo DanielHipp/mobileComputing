c6c23420f9f6168001dcbfe9636ed6731d908356
Thomas <thgnaedi@htwg-konstanz.de>
2018-12-17 17:05:00 +0100

    insert both LED-Pannels, and insert Image

--- Test/demo_matrix.py
+++ Test/demo_matrix.py
@@ -1,4 +1,5 @@
 import sys
+import numpy as np
 
 from Adafruit_LED_Backpack import Matrix8x16
 from matrix16x16 import Matrix16x16
@@ -9,11 +10,35 @@ if len(sys.argv) > 1:
 else:
 	txt="ABCDEFGHIJKLMNO P"
 print("Ausgabe sollte sein:",txt)
+
 display = Matrix8x16.Matrix8x16(address=0x70)
 display.begin()
 display.set_brightness(3)
 
-matrix = Matrix16x16(display, None)
+displaylow = Matrix8x16.Matrix8x16(address=0x71)
+displaylow.begin()
+displaylow.set_brightness(3)
+
+matrix = Matrix16x16(display, displaylow)
 matrix.write_string(txt)
 
-
+example_img=np.array([
+	[0,0,0,0,1,1,1,1,1,1,1,1,0,0,0,0],
+	[0,0,0,1,1,0,0,0,0,0,1,1,1,0,0,0],
+	[0,0,1,1,1,1,1,1,1,1,1,1,1,1,0,0],
+	[0,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0],
+	[0,1,1,1,0,0,0,0,0,0,0,1,1,1,0,0],
+	[0,1,1,1,0,0,0,0,0,0,0,1,1,1,0,0],
+	[0,1,1,0,0,0,0,0,0,0,0,0,1,1,0,0],
+	[1,1,1,0,1,1,0,0,0,1,1,0,1,0,1,0],
+	[1,0,1,0,1,1,0,0,0,1,1,0,1,0,1,0],
+	[1,1,1,0,1,1,0,0,0,1,1,0,1,1,1,0],
+	[0,1,1,0,0,0,0,0,0,0,0,0,1,1,0,0],
+	[0,1,1,0,0,0,0,0,0,0,0,0,1,1,0,0],
+	[0,0,1,1,0,1,0,0,0,1,0,1,1,0,0,0],
+	[0,0,0,1,0,0,1,1,1,0,0,1,0,0,0,0],
+	[0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0],
+	[0,0,0,0,0,1,1,1,1,1,0,0,0,0,0,0],
+])
+matrix.draw_Image(example_img)
+print("finished, to clean Matrix run $python clearMatrix.py")
