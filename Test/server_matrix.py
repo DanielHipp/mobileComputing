914f46601e235ec3ac9916c46ab7e37b814e07b2
thgnaedi <thgnaedi@htwg-konstanz.de>
2018-12-23 11:50:05 +0000

    changed to Python2

--- Test/server_matrix.py
+++ Test/server_matrix.py
@@ -1,9 +1,10 @@
-from http.server import HTTPServer, BaseHTTPRequestHandler
+#from http.server import HTTPServer, BaseHTTPRequestHandler
+import BaseHTTPServer
 import stringHandler
 from matrix16x16 import Matrix16x16
 
 
-class RequestHandler(BaseHTTPRequestHandler):
+class RequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
 
     def do_GET(self):
         self.send_response(200)
@@ -20,7 +21,7 @@ class RequestHandler(BaseHTTPRequestHandler):
 
 if __name__ == '__main__':
     port = 8000
-    MATRIX_AVALIABLE = False
+    MATRIX_AVALIABLE = True
     #Set MATRIX_AVALIABLE to True if this script runs on your Raspberry
     if MATRIX_AVALIABLE:
         from Adafruit_LED_Backpack import Matrix8x16
@@ -41,5 +42,5 @@ if __name__ == '__main__':
     STRINGHANDLER = stringHandler.StringHandler(imgChar='I', stringChar='S', imgSize=16 * 16, matrix16x16=matrix)
 
     print("server Running on Port",port)
-    server = HTTPServer(('',port), RequestHandler)
+    server = BaseHTTPServer.HTTPServer(('',port), RequestHandler)
     server.serve_forever()
