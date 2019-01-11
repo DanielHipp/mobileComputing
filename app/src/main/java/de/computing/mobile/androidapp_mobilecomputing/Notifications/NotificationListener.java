79fdd29ab814630e09157964761e0fc32c5dc441
Daniel Hipp <dahipp@htwg-konstanz.de>
2019-01-11 16:12:24 +0100

    removed useless code

--- app/src/main/java/de/computing/mobile/androidapp_mobilecomputing/Notifications/NotificationListener.java
+++ app/src/main/java/de/computing/mobile/androidapp_mobilecomputing/Notifications/NotificationListener.java
@@ -32,15 +32,6 @@ public class NotificationListener extends NotificationListenerService {
 
     @Override
     public void onNotificationPosted(StatusBarNotification sbn){
-        String pack =sbn.getPackageName();
-        String ticker = "";
-        if(sbn.getNotification().tickerText != null){
-            ticker = sbn.getNotification().tickerText.toString();
-        }
-        Bundle extras = sbn.getNotification().extras;
-        String title = extras.getString("android.title");
-        String text = extras.getCharSequence("android.text").toString();
-        int iconSmall = extras.getInt(Notification.EXTRA_SMALL_ICON);
         Icon icon = sbn.getNotification().getLargeIcon();
 
         Bitmap bitmap = drawableToBitmap(icon.loadDrawable(context));
